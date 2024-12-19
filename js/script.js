67734386b03951b56024edd447742a2f5f9d8c32
Damian Gurdak <damiangurdakk@gmail.com>
2024-12-19 21:16:38 +0100

    develop logic game

--- js/script.js
+++ js/script.js
@@ -1,31 +1,42 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+function getMoveName(moveId) {
+  if (moveId == 1) {
+    return 'kamień';
+  } else if (moveId == 2) {
+    return 'papier';
+  } else if (moveId == 3) {
+    return 'nożyce';
+  } else {
+    printMessage('Nie znam ruchu o id ' + moveId + '.');
+    return 'nieznany ruch';
+  }
+}
 
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
-if (randomNumber == 1) {
-  computerMove = 'kamień';
-} else if (randomNumber == 2) {
-  computerMove = 'papier';
-} else if (randomNumber == 3) {
-  computerMove = 'nożyce';
-}
-
-printMessage('Mój ruch to: ' + computerMove);
-
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
+let playerMove = getMoveName(playerInput);
 
-if (playerInput == '1') {
-  playerMove = 'kamień';
-} else if (playerInput == '2') {
-  playerMove = 'papier';
-} else if (playerInput == '3') {
-  playerMove = 'nożyce';
+function displayResult(computerMove, playerMove) {
+  console.log('moves:', computerMove, playerMove);
+  printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
+
+  if (
+    (computerMove == 'kamień' && playerMove == 'papier') ||
+    (computerMove == 'nożyce' && playerMove == 'kamień') ||
+    (computerMove == 'papier' && playerMove == 'nożyce')
+  ) {
+    printMessage('Ty wygrywasz!');
+  } else if (computerMove == playerMove) {
+    printMessage('Mamy remis! Spróbój jeszcze raz!');
+  } else if (playerMove == 'nieznany ruch') {
+    printMessage('Twój ruch jest błędny! Spróbój jeszcze raz!');
+  } else {
+    printMessage('Tym razem przegrywasz :(');
+  }
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+displayResult(computerMove, playerMove);
